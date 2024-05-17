0aba0bbb62060382147f3ebd947627f80bdde252
Rohan-Muslekar <rohnabhi20vt@gmail.com>
2024-05-17 20:09:00 +0530

    chore: submodules updated | compiled cs out

--- src/gen.sh
+++
@@ -1,2 +0,0 @@
-protoc -I=./proto --cpp_out=./proto --csharp_out=./ proto/payload.proto --experimental_allow_proto3_optional
-protoc -I=./proto --cpp_out=./proto --csharp_out=./ proto/match.proto --experimental_allow_proto3_optional
